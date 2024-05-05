d2b8490e9f5dccbaf47c3456da9ce3c4c35bd36f
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-05-04 22:53:05 -0300

    :wrench: Modificando h1 do index.html

---
+++ texto-bolha.js
@@ -0,0 +1,17 @@
+function dividirTexto(target) {
+  let elements = document.querySelectorAll(target)
+  elements.forEach((element) => {
+    element.classList.add('split-text')
+    let text = element.innerText
+    let splitText = text
+    .split(" ")
+    .map(function (word) {
+      let char = word.split('').map(char => {
+        return `<span class="split-char">${char}</span>`}).join('')
+        return `<div class="split-word">${char}&nbsp</div>`}).join('')
+
+        element.innerHTML = splitText;
+  })
+}
+
+dividirTexto('.texto-bolha')
