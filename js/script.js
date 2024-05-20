88d48c44c4a0bf7137aa9c3d3120b4565e45324a
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-05-19 21:14:31 -0300

    :wrench: Animação de entrada da página inicial do site

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ const galeriaContainer = document.querySelector(".bicicleta-imagens");
 function trocarImagem(event) {
   const img = event.currentTarget;
   const media = matchMedia("(min-width: 1000px)").matches;
-  if(media) {
+  if (media) {
     galeriaContainer.prepend(img);
   }
 }
@@ -60,3 +60,8 @@ function eventosGaleria(img) {
 }
 
 galeria.forEach(eventosGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
